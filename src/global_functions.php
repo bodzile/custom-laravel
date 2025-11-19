91a94d13997bb1b86b362c33535523144535fbb2
bodzile <155837912+bodzile@users.noreply.github.com>
2025-11-19 18:08:15 +0100

    [GLOBALFUNCTIONS-01] - Create Global functions for project

---
+++ src/global_functions.php
@@ -0,0 +1,56 @@
+<?php
+
+use Src\Database;
+use Src\Project;
+use Routes\Route;
+
+require_once "vendor/autoload.php";
+
+enum Purpose:string {
+    case SINGLE_USE ="single";
+}
+
+function getDatabaseParam()
+{
+    return Database::$db_info;
+}
+
+function root()
+{
+    return Project::$project_name;
+}
+
+function route($route_name, array $param=[])
+{
+    $value_passed="";
+    if(!empty($param))
+    {
+        $value_passed=$param[array_key_first($param)];
+        //die($value);
+    }
+    foreach(Route::$links as $path => $value)
+    {
+        if( isset($value["name"]))
+        {
+            if($value["name"] == $route_name)
+            {
+                if($value_passed != "")
+                    return root() . $path . $value_passed ;
+                return root() . $path;
+            }
+        }
+    }
+
+    return null;
+}
+
+function compact2(...$keys)
+{
+    $result=[];
+    foreach($keys as $key)
+    {
+        global $$key;
+        $result[$key] = $$key;
+    }
+    return $result;
+}
