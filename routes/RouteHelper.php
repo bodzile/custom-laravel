f2cd392a097dc35e8c67a9147784fba54b8d6a9d
bodzile <155837912+bodzile@users.noreply.github.com>
2025-11-19 18:18:39 +0100

    [ROUTE-02] - Create RouteHelper class for helper functions

---
+++ routes/RouteHelper.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace Routes;
+
+class RouteHelper
+{
+    public static function extractPath(string $path):string
+    {
+        //input "/neko ime/admin/user", output "/admin/user"
+        //input "/projekat/admin/add-user", output "/admin/add-user"
+        //prvo split po "/" i onda ici po nizu od indeksa 1 i dodavati "/" i vratiti taj string
+        
+        $result="";
+        $split=explode("/",$path);
+
+        for($i=2;$i<count($split);$i++)
+        {
+            $result =$result .  "/" . $split[$i];
+        }
+        
+        return $result;
+    }
+
+    public static function cutGetFromUrl(string $path)
+    {
+        return explode("?",$path)[0];
+    }
+
+    public static function getValueFromUrl(string $path)
+    {
+        $temp=explode("/",$path);
+        //print_r($temp); 
+        //echo "<br>" . count($temp)-1;
+        //die();
+        return $temp[count($temp)-1];
+    }
+
+    public static function cutValueFromUrl(string $path)
+    {
+        $result="";
+        $temp=explode("/",$path);
+        //print_r($temp);
+        //die();
+        
+        for($i=1;$i<count($temp)-1;$i++)
+        {
+            $result=$result . "/"  . $temp[$i];
+            //die($result);
+        }
+
+        $result.="/";
+        
+        return $result;
+    }
+
+    public static function cutValueFromUrlBrackets(string $path)
+    {
+        $result = preg_replace('/\s*\{.*?\}\s*/', ' ', $path);
+        $result = trim($result); // To clean up any leading or trailing spaces.
+        return $result;
+    }
+
+    public static function getValueNameFromUrl(string $path)
+    {
+        if(preg_match("/\{(.*?)\}/",$path,$matches))
+        {
+            return $matches[1];
+        }
+        return null;
+    }
+}
