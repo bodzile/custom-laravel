1bf56851b931cd2f5a62beb5160ff1a22830f42c
bodzile <155837912+bodzile@users.noreply.github.com>
2025-11-19 18:18:52 +0100

    [ROUTE-03] - Create web.php for testing of Route methods

---
+++ routes/web.php
@@ -0,0 +1,35 @@
+<?php
+use Routes\Route;
+
+require_once "vendor/autoload.php";
+
+
+//Route::view("/","welcome")
+  //  ->name("home");
+
+Route::get("/","MyController","index2")   
+  ->name("home");
+
+Route::get("/admin/test","MyController","index")
+  ->name("admin.testPage");
+
+Route::post("/admin/add-user","MyController","addUser")
+  ->name("admin.addUser");
+
+Route::post("/admin/delete/{id}","MyController","delete")
+  ->name("admin.deleteUser");
+
+Route::middleware("auth")->prefix("/admin/rnd")->group(function(){
+
+  Route::get("/random","MyController","index")
+    ->name("admin.testPage2");
+
+  Route::get("/random2","MyController","index2")
+    ->name("admin.addUser2");
+
+});
+
+Route::get("/admin/nesto","MyController","index");
+
+
+
