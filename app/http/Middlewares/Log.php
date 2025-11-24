917edb653ef59479ac28151effbadee39ad062e3
bodzile <155837912+bodzile@users.noreply.github.com>
2025-11-24 23:47:58 +0100

    [FEATURE][MIDDLEWARE-01] - Added middleware interface and middleware classes for test

---
+++ app/http/Middlewares/Log.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Middlewares;
+
+use App\Http\Requests\Request;
+use Src\RedirectTrait;
+
+class Log implements Middleware
+{
+
+    use RedirectTrait;
+
+    public static function handle(Request $request, \Closure $next)
+    {
+
+        if(1==2)
+        {
+
+            return redirect("/");
+               // header("Location: google.com");
+             
+        }
+
+
+        return $next($request);
+    }
+}
