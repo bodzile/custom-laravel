7eddcccb06b1b3a8d60c568a753bd82377eb6df4
bodzile <155837912+bodzile@users.noreply.github.com>
2025-11-24 23:48:57 +0100

    [FEATURE][MIDDLEWARE-01] - Added Pipeline class modified for middlewares only

---
+++ src/Pipeline.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Src;
+
+class Pipeline{
+
+    private $data;
+    private $pipes=[];
+
+    public static function send($data):Pipeline
+    {
+        $pipeline=new self;
+        $pipeline->data=$data;
+
+        return $pipeline;
+    }
+
+    public function through(array $functions):Pipeline
+    {
+        $this->pipes=$functions;
+        return $this;
+    }
+
+    public function to(\Closure $destination)
+    {
+        $next=$destination;
+        $value=$this->data;
+
+        foreach(array_reverse($this->pipes) as $pipe)
+        {
+            //$resolved=$this->resolve($pipe);
+            $next=function($value) use ($pipe,$next){
+                return call_user_func([$pipe,"handle"],$value,$next);
+            };
+        }
+
+        return $next($this->data);
+    }
+
+}
