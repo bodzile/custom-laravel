cd67df10159d426401f357347e29cada9031e20c
bodzile <155837912+bodzile@users.noreply.github.com>
2025-12-03 06:25:17 +0100

    [FEATURE][MODEL-04] - Added all model functions

--- app/models/Model.php
+++ app/models/Model.php
@@ -11,17 +11,60 @@ abstract class Model extends Database
     protected static string $table;
     protected static array $allowed;
 
+    //polja koja ce se kreirati kao posebne varijable u child klasi
+    protected array $fields;
 
     public static function query():QueryBuilder
     {
         static::$pdo=static::getConnection();
-        return new QueryBuilder(static::$table,static::$pdo,static::$allowed);
+        return static::createQueryBuilder();
     }
     
     public static function create(array|Request $data):bool
     {
         static::$pdo=static::getConnection();
-        return new QueryBuilder(static::$table,static::$pdo,static::$allowed)->create($data);
+        return static::createQueryBuilder()->create($data);
+    }
+
+    public static function find(int $id):Object
+    {
+        static::$pdo=static::getConnection();
+        return static::createQueryBuilder()->getById($id);
+    }
+
+    public static function update(int $id, array $values):bool
+    {
+        static::$pdo=static::getConnection();
+        return static::createQueryBuilder()->update($id, $values);
+    }
+
+    public function delete():bool
+    {
+        static::$pdo=static::getConnection();
+        return static::createQueryBuilder()->delete((int)$this->id);
+    }
+
+    public function save():bool
+    {
+        static::$pdo=static::getConnection();
+        return static::createQueryBuilder()->update((int)$this->id, $this->fields);
+    }
+
+    private static function createQueryBuilder()
+    {
+        return new QueryBuilder(static::$table, static::$pdo, static::class, static::$allowed);
+    }
+
+    public function __set($name, $value)
+    {
+        if(!in_array($name,static::$allowed) && !in_array($name,["id","created_at","updated_at"]))
+            return;
+        $this->fields[$name]=$value;
+    }
+
+    public function __get($name)
+    {
+        return $this->fields[$name];
     }
 
 }
